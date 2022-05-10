9778a5f45981436cd97bf4ded34b25a937859dc2
lkxed <lkxed@outlook.com>
2022-05-10 16:33:55 +0800

    replace more quotes with `-`

--- internal/collector/generator_test.go
+++ internal/collector/generator_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestGenerate(t *testing.T) {
-	article := Parse("https://news.itsfoss.com/tails-5-0-release/")
+	article := Parse("https://itsfoss.com/cant-run-appimage-ubuntu/")
 	filepath, content := Generate(article)
 	fmt.Println(filepath)
 	fmt.Println(string(content))
