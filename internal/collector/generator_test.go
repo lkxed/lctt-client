18e81e401898fbf57c47f061b7d90410baa16ab9
lkxed <lkxed@outlook.com>
2022-05-19 14:27:40 +0800

    exclude <a> tags inside <amp-youtube> tags

--- internal/collector/generator_test.go
+++ internal/collector/generator_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestGenerate(t *testing.T) {
-	article := Parse("https://www.debugpoint.com/2022/01/best-gnome-apps-part-3/")
+	article := Parse("https://news.itsfoss.com/inkscape-1-2-release/")
 	filepath, content := Generate(article)
 	fmt.Println(filepath)
 	fmt.Println(string(content))
